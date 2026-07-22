170ce631781a80703545f81f7b10979444bed05f
DRC <information@libjpeg-turbo.org>
2026-07-22 15:21:51 -0400

    tj3*Image*(): Copy libjpeg err to primary instance
    If the libjpeg instance associated with the secondary TurboJPEG instance
    in tj3LoadImage*() or tj3SaveImage*() throws an error, then the error
    message and state will be contained within the secondary TurboJPEG
    instance.  We need to copy it to the primary TurboJPEG instance so that
    tj3GetErrorStr() and tj3GetErrorCode() work properly.

--- turbojpeg-mp.c
+++ turbojpeg-mp.c
@@ -319,7 +319,7 @@ _JSAMPLE *GET_NAME(_tj3LoadImageFromFileHandle, BITS_IN_JSAMPLE)
   else if (tempc == EOF)
     THROW("Input file contains no data");
 
-  CATCH_LIBJPEG(this2);
+  CATCH_LIBJPEG2();
 
   cinfo->data_precision = BITS_IN_JSAMPLE;
   if (*pixelFormat == TJPF_UNKNOWN) cinfo->in_color_space = JCS_UNKNOWN;
@@ -363,7 +363,7 @@ _JSAMPLE *GET_NAME(_tj3LoadImageFromFileHandle, BITS_IN_JSAMPLE)
                                    sizeof(_JSAMPLE))) == NULL)
     THROW("Memory allocation failure");
 
-  CATCH_LIBJPEG(this2);
+  CATCH_LIBJPEG2();
 
   while (cinfo->next_scanline < cinfo->image_height) {
     int i, nlines = (*src->get_pixel_rows) (cinfo, src);
@@ -483,7 +483,7 @@ DLLEXPORT int GET_NAME(tj3SaveImage, BITS_IN_JSAMPLE)
 #endif
     THROW_UNIX("Cannot open output file");
 
-  CATCH_LIBJPEG(this2);
+  CATCH_LIBJPEG2();
 
   this2->dinfo.out_color_space = pf2cs[pixelFormat];
   dinfo->image_width = width;  dinfo->image_height = height;
