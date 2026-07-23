hexagon-geo-surv/libjpeg-turbo

commit 170ce631781a80703545f81f7b10979444bed05f

tj3*Image*(): Copy libjpeg err to primary instance
If the libjpeg instance associated with the secondary TurboJPEG instance
in tj3LoadImage*() or tj3SaveImage*() throws an error, then the error
message and state will be contained within the secondary TurboJPEG
instance.  We need to copy it to the primary TurboJPEG instance so that
tj3GetErrorStr() and tj3GetErrorCode() work properly.

turbojpeg.c
@@ -282,6 +282,18 @@ static int cs2pf[JPEG_NUMCS] = {
   } \
 }
 
+/* Catch a libjpeg error from the secondary TurboJPEG instance and copy the
+   error message and state to the primary instance.  This is used by
+   the packed-pixel image I/O functions. */
+#define CATCH_LIBJPEG2() { \
+  if (setjmp(this2->jerr.setjmp_buffer)) { \
+    strncpy(this->errStr, this2->errStr, JMSG_LENGTH_MAX); \
+    this->jerr.warning = this2->jerr.warning; \
+    this->isInstanceError = this2->isInstanceError; \
+    retval = -1;  goto bailout; \
+  } \
+}
+
 #define GET_INSTANCE(handle) \
   tjinstance *this = (tjinstance *)handle; \
   j_compress_ptr cinfo = NULL; \

turbojpeg-mp.c
@@ -319,7 +319,7 @@ _JSAMPLE *GET_NAME(_tj3LoadImageFromFileHandle, BITS_IN_JSAMPLE)
   else if (tempc == EOF)
     THROW("Input file contains no data");
 
-  CATCH_LIBJPEG(this2);
+  CATCH_LIBJPEG2();
 
   cinfo->data_precision = BITS_IN_JSAMPLE;
   if (*pixelFormat == TJPF_UNKNOWN) cinfo->in_color_space = JCS_UNKNOWN;
@@ -363,7 +363,7 @@ _JSAMPLE *GET_NAME(_tj3LoadImageFromFileHandle, BITS_IN_JSAMPLE)
                                    sizeof(_JSAMPLE))) == NULL)
     THROW("Memory allocation failure");
 
-  CATCH_LIBJPEG(this2);
+  CATCH_LIBJPEG2();
 
   while (cinfo->next_scanline < cinfo->image_height) {
     int i, nlines = (*src->get_pixel_rows) (cinfo, src);
@@ -483,7 +483,7 @@ DLLEXPORT int GET_NAME(tj3SaveImage, BITS_IN_JSAMPLE)
 #endif
     THROW_UNIX("Cannot open output file");
 
-  CATCH_LIBJPEG(this2);
+  CATCH_LIBJPEG2();
 
   this2->dinfo.out_color_space = pf2cs[pixelFormat];
   dinfo->image_width = width;  dinfo->image_height = height;

commit 0f69838b1fa98397699b22a3b9c4a4678cafb21a

turbojpeg.c: Fix Windows build
(broken by 170ce631781a80703545f81f7b10979444bed05f)

strncpy() is verboten.

turbojpeg.c
@@ -287,7 +287,7 @@ static int cs2pf[JPEG_NUMCS] = {
    the packed-pixel image I/O functions. */
 #define CATCH_LIBJPEG2() { \
   if (setjmp(this2->jerr.setjmp_buffer)) { \
-    strncpy(this->errStr, this2->errStr, JMSG_LENGTH_MAX); \
+    memcpy(this->errStr, this2->errStr, JMSG_LENGTH_MAX); \
     this->jerr.warning = this2->jerr.warning; \
     this->isInstanceError = this2->isInstanceError; \
     retval = -1;  goto bailout; \