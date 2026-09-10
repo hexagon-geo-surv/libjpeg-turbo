hexagon-geo-surv/libjpeg-turbo

commit 8fdda56ef8a36eacd59778c6eaa735008a0453db

Dec: Fix buf overrun w/4:2:0 merged/!SIMD/crop w<2
When partially decompressing a 4:2:0 JPEG image using merged (non-fancy)
upsampling, reinit_upsampler was set to TRUE if jpeg_crop_scanline() was
called with arguments that would have caused any of the component planes
to be cropped to a width of 1 or 2 samples.  However, that behavior was
incorrect, because only the non-merged upsampler needs to be
reinitialized if compptr->downsampled_width changes.  Furthermore,
attempting to reinitialize the non-merged upsampler when the merged
upsampler was already initialized caused a buffer overrun because the
opaque non-merged upsampler structure is significantly larger than the
opaque merged upsampler structure.

On certain platforms (reproducible with Linux but not macOS), the buffer
overrun corrupted the lower part of the Cr-to-R conversion table.  If a
Cr value was small enough, then h2v2_merged_upsample*() read one of the
bogus values from the corrupted part of the table, causing an
out-of-bounds array access and subsequent segfault when attempting to
use the value as an index into the cinfo->sample_range_limit array.  In
most cases, however, the buffer overrun was contained, so the issue went
undetected, even by ASan.

This issue was only reproducible with the libjpeg-turbo SIMD extensions
disabled, because none of the h2v2 merged upsampling SIMD modules
accesses the aforementioned opaque structures.  Thus, the scope of the
issue was very limited, given that very few downstream applications
disable the SIMD extensions.  That, in addition to the fact that the
issue was platform-dependent and image-dependent, explains why it has
thus far gone unnoticed, even though the code in question has existed
for 11 years (since libjpeg-turbo 1.5.x.)

jdapistd.c
@@ -279,7 +279,8 @@ _jpeg_crop_scanline(j_decompress_ptr cinfo, JDIMENSION *xoffset,
                                        compptr->_DCT_scaled_size),
                                 (long)(cinfo->max_h_samp_factor *
                                        cinfo->_min_DCT_scaled_size));
-    if (compptr->downsampled_width < 2 && orig_downsampled_width >= 2)
+    if (compptr->downsampled_width < 2 && orig_downsampled_width >= 2 &&
+        !master->using_merged_upsample)
       reinit_upsampler = TRUE;
 
     /* Set the first and last iMCU columns that we must decompress.  These

commit 9ed08afd717e6ed54de23c2e47c60d062da76c72

Fix build if UPSAMPLE_MERGING_SUPPORTED undefined
(broken by 8fdda56ef8a36eacd59778c6eaa735008a0453db)

jdapistd.c
@@ -279,8 +279,11 @@ _jpeg_crop_scanline(j_decompress_ptr cinfo, JDIMENSION *xoffset,
                                        compptr->_DCT_scaled_size),
                                 (long)(cinfo->max_h_samp_factor *
                                        cinfo->_min_DCT_scaled_size));
-    if (compptr->downsampled_width < 2 && orig_downsampled_width >= 2 &&
-        !master->using_merged_upsample)
+    if (compptr->downsampled_width < 2 &&
+#ifdef UPSAMPLE_MERGING_SUPPORTED
+        !master->using_merged_upsample &&
+#endif
+        orig_downsampled_width >= 2)
       reinit_upsampler = TRUE;
 
     /* Set the first and last iMCU columns that we must decompress.  These